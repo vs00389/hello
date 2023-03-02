e6aa20547a775e1a7af0b57e10baf959939887bf
KANNAN <KANNAN@DESKTOP-5JR1SAH>
2023-03-03 02:00:14 +0530

    first commit

---
+++ HelloWorld/src/main/java/com/javatpoint/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.javatpoint;
+
+public class HelloWorld {
+	   
+		    public static void main(String [] argv) // this is the entry point of program
+	    {
+	        System.out.println("First Java Azure Batch!");
+	    }
+	
+}
